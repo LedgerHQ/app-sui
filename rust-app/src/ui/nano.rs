1a1b2cef7754401bd8089952b9f86071b1ca626e
Divam <dfordivam@gmail.com>
2025-02-28 21:39:47 +0900

    Change nano device UI to conform to new designs
    And use gadgets from SDK in place of prompts_ui APIs

--- rust-app/src/ui/nano.rs
+++ rust-app/src/ui/nano.rs
@@ -1,18 +1,40 @@
 use crate::interface::*;
 use crate::utils::*;
-use core::fmt::Write;
+
+extern crate alloc;
+use alloc::format;
+
 use ledger_crypto_helpers::common::HexSlice;
 use ledger_crypto_helpers::hasher::HexHash;
-use ledger_prompts_ui::*;
+
+use ledger_device_sdk::buttons::ButtonEvent;
+use ledger_device_sdk::ui::bitmaps::{CHECKMARK, CROSS, EYE, WARNING};
+use ledger_device_sdk::ui::gadgets::*;
 
 #[derive(Copy, Clone)]
 pub struct UserInterface {}
 
 impl UserInterface {
     pub fn confirm_address(&self, address: &SuiPubKeyAddress) -> Option<()> {
-        scroller("Provide Public Key", |_w| Ok(()))?;
-        scroller_paginated("Address", |w| Ok(write!(w, "{address}")?))?;
-        final_accept_prompt(&[])
+        let fields = [Field {
+            name: "Address",
+            value: &format!("{address}"),
+        }];
+        let success = MultiFieldReview::new(
+            &fields,
+            &[&"Provide Public Key"],
+            None,
+            &"Approve",
+            None,
+            &"Reject",
+            None,
+        )
+        .show();
+        if success {
+            Some(())
+        } else {
+            None
+        }
     }
 
     pub fn confirm_sign_tx(
@@ -22,71 +44,74 @@ impl UserInterface {
         total_amount: u64,
         gas_budget: u64,
     ) -> Option<()> {
-        scroller("Transfer", |w| Ok(write!(w, "SUI")?))?;
-
-        scroller_paginated("From", |w| Ok(write!(w, "{address}")?))?;
-        scroller_paginated("To", |w| Ok(write!(w, "0x{}", HexSlice(&recipient))?))?;
-
-        let (quotient, remainder_str) = get_amount_in_decimals(total_amount);
-        scroller_paginated("Amount", |w| {
-            Ok(write!(w, "SUI {quotient}.{}", remainder_str.as_str())?)
-        })?;
-
-        let (quotient, remainder_str) = get_amount_in_decimals(gas_budget);
-        scroller("Max Gas", |w| {
-            Ok(write!(w, "SUI {}.{}", quotient, remainder_str.as_str())?)
-        })?;
-        final_accept_prompt(&["Sign Transaction?"])
+        let fields = [
+            Field {
+                name: "From",
+                value: &format!("{address}"),
+            },
+            Field {
+                name: "To",
+                value: &format!("0x{}", HexSlice(&recipient)),
+            },
+            Field {
+                name: "Amount",
+                value: {
+                    let (quotient, remainder_str) = get_amount_in_decimals(total_amount);
+                    &format!("SUI {}.{}", quotient, remainder_str.as_str())
+                },
+            },
+            Field {
+                name: "Max Gas",
+                value: {
+                    let (quotient, remainder_str) = get_amount_in_decimals(gas_budget);
+                    &format!("SUI {}.{}", quotient, remainder_str.as_str())
+                },
+            },
+        ];
+        let success = MultiFieldReview::new(
+            &fields,
+            &[&"Review", &"transaction"],
+            Some(&EYE),
+            &"Accept and send",
+            Some(&CHECKMARK),
+            &"Reject",
+            Some(&CROSS),
+        )
+        .show();
+        if success {
+            Some(())
+        } else {
+            None
+        }
     }
 
     pub fn confirm_blind_sign_tx(&self, hash: &HexHash<32>) -> Option<()> {
-        scroller("WARNING", |w| Ok(write!(w, "Transaction not recognized")?))?;
-        scroller("Transaction Hash", |w| Ok(write!(w, "0x{hash}")?))?;
-        final_accept_prompt(&["Blind Sign Transaction?"])
+        let fields = [Field {
+            name: "Transaction hash",
+            value: &format!("0x{hash}"),
+        }];
+        let success = MultiFieldReview::new(
+            &fields,
+            &[&"WARNING transaction", &"not recognized"],
+            Some(&WARNING),
+            &"Accept and send",
+            Some(&CHECKMARK),
+            &"Reject",
+            Some(&CROSS),
+        )
+        .show();
+        if success {
+            Some(())
+        } else {
+            None
+        }
     }
 
     pub fn warn_tx_not_recognized(&self) {
-        scroller("WARNING", |w| {
-            Ok(write!(
-                w,
-                "Transaction not recognized, enable blind signing to sign unknown transactions"
-            )?)
-        });
+        let field = Field {
+            name: "WARNING",
+            value: "transaction not recognized, enable blind signing to sign unknown transactions",
+        };
+        field.event_loop(ButtonEvent::RightButtonRelease, true);
     }
-}
-
-#[cfg(not(target_os = "nanos"))]
-#[inline(never)]
-pub fn scroller<F: for<'b> Fn(&mut PromptWrite<'b, 16>) -> Result<(), ScrollerError>>(
-    title: &str,
-    prompt_function: F,
-) -> Option<()> {
-    ledger_prompts_ui::write_scroller_three_rows(false, title, prompt_function)
-}
-
-#[cfg(target_os = "nanos")]
-#[inline(never)]
-pub fn scroller<F: for<'b> Fn(&mut PromptWrite<'b, 16>) -> Result<(), ScrollerError>>(
-    title: &str,
-    prompt_function: F,
-) -> Option<()> {
-    ledger_prompts_ui::write_scroller(false, title, prompt_function)
-}
-
-#[cfg(not(target_os = "nanos"))]
-#[inline(never)]
-pub fn scroller_paginated<F: for<'b> Fn(&mut PromptWrite<'b, 16>) -> Result<(), ScrollerError>>(
-    title: &str,
-    prompt_function: F,
-) -> Option<()> {
-    ledger_prompts_ui::write_scroller_three_rows(true, title, prompt_function)
-}
-
-#[cfg(target_os = "nanos")]
-#[inline(never)]
-pub fn scroller_paginated<F: for<'b> Fn(&mut PromptWrite<'b, 16>) -> Result<(), ScrollerError>>(
-    title: &str,
-    prompt_function: F,
-) -> Option<()> {
-    ledger_prompts_ui::write_scroller(true, title, prompt_function)
 }
